4c4cbdb16877191117e3a635b7a3e31ee65bba70
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-21 17:52:53 +0530

    [Raja] Added UC4EmployeeWage implimented in Employee Wage

--- EmployeeWages/EmployeeWage.cs
+++ EmployeeWages/EmployeeWage.cs
@@ -13,31 +13,33 @@ namespace EmployeeWages
         /// </summary>
         public static void DisplayEmployeeWage() //creating method for a class
         {
-            int isFullTime = 1;
-            int isPartTime = 2;
+            const int Is_Full_Time = 1;  //cretaing a constants value by using the const key word
+            const int Is_Part_Time = 2;
             int empHrs = 0;
-            int empRatePerHr = 20;
+            const int Emp_Rate_Per_Hr = 20;
             int employeeWage = 0;
-            Random random = new Random();   // creating a object for a random value
-            int empCheck = random.Next(0, 3);   // to get the values randomly between 0 to 2
-            if (empCheck == isFullTime)   //If random value 1 then it will execute the block statemnt
+            Random random = new Random();  // creating a object for a random value
+            int empCheck = random.Next(0, 3);  // to get the values randomly between 0 to 2
+            switch (empCheck)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is Working Full time");
-                if (empCheck == isPartTime)   //If random value 2 then it will execute the block statemnt
-                {
+                case Is_Full_Time:  //If random value 1 then it will execute the block statemnt
+                    empHrs = 8;
+                    Console.WriteLine("Employee is working full time");
+                    break;
+                case Is_Part_Time:  //If random value 2 then it will execute the block statemnt
                     empHrs = 4;
-                    Console.WriteLine("Employee is Working Part  time");
+                    Console.WriteLine("Employee is working part time");
 
-                }
+                    break;
+                default:   //If random value 0 then it will execute the block statemnt
+                    empHrs = 0;
+                    Console.WriteLine("Employee is absent");
 
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
-            employeeWage = empHrs * empRatePerHr;
-            Console.WriteLine("Employee Wage:" + employeeWage);
+            // claculation part for a employee wage
+            employeeWage = empHrs * Emp_Rate_Per_Hr;
+            Console.WriteLine("Employee Wage :" + employeeWage);
         }     
     }
 }
