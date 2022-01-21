3e2c34165a5f62847bc0d4042d182acca08a6447
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-21 17:11:27 +0530

    [Raja] Added UC1EmployeeWage implimented in Employee Wage

---
+++ EmployeeWages/EmployeeWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeWage
+    {
+        public static void DisplayEmployeeWage()
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -10,7 +10,7 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWage.DisplayEmployeeWage();
         }
     }
 }
