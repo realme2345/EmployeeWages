3cd212051e1f97290b6f2884d9cc6c7ac70da301
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-21 17:49:24 +0530

    [Raja] Added UC3EmployeeWage implimented in Employee Wage

--- EmployeeWages/EmployeeWage.cs
+++ EmployeeWages/EmployeeWage.cs
@@ -13,26 +13,31 @@ namespace EmployeeWages
         /// </summary>
         public static void DisplayEmployeeWage() //creating method for a class
         {
-            
-                int isFullTime = 1;
-                int empHrs = 0;
-                int empRatePerHr = 20;
-                int employeeWage = 0;
-                Random random = new Random();
-                int empCheck = random.Next(0, 2);
-                if (empCheck == isFullTime)
+            int isFullTime = 1;
+            int isPartTime = 2;
+            int empHrs = 0;
+            int empRatePerHr = 20;
+            int employeeWage = 0;
+            Random random = new Random();   // creating a object for a random value
+            int empCheck = random.Next(0, 3);   // to get the values randomly between 0 to 2
+            if (empCheck == isFullTime)   //If random value 1 then it will execute the block statemnt
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee is Working Full time");
+                if (empCheck == isPartTime)   //If random value 2 then it will execute the block statemnt
                 {
-                    empHrs = 8;
-                    Console.WriteLine("Employee is present");
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Working Part  time");
 
                 }
-                else
-                {
-                    Console.WriteLine("Employee is absent");
-                }
-                employeeWage = empHrs * empRatePerHr; //Gett the total employee wage
-                Console.WriteLine("Employee Wage:" + employeeWage);
-            
-        }
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            employeeWage = empHrs * empRatePerHr;
+            Console.WriteLine("Employee Wage:" + employeeWage);
+        }     
     }
 }
