c6557baa90e1e41cca090a035ebcb6ce01c0746f
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-23 19:38:40 +0530

    [Raja] Added UC10EmployeeWage implimented in EmployeeWages

--- EmployeeWages/EmployeeWage.cs
+++ EmployeeWages/EmployeeWage.cs
@@ -34,35 +34,30 @@ namespace EmployeeWages
         }
         private int computeEmpWage(CompanyEmpWage companyEmpWage)
         {
+
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 1;
+            while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
             {
-                int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 1;
-                while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+                Random random = new Random(); //Creating a object for random value
+                int empCheck = random.Next(0, 3);// To get the values from 0 to 2
+                switch (empCheck)
                 {
-                    //totalWorkingDays++;
-                    Random random = new Random(); //Creating a object for random value
-                    int empCheck = random.Next(0, 3);// To get the values from 0 to 2
-                    switch (empCheck)
-                    {
-                        case Is_Full_Time: // If random value is 1 it will execute block of statement
-                            empHrs = 8;
-                            // Console.WriteLine("{0} day Employee is working full time", day);
-                            break;
-                        case Is_Part_Time: // If random value is 2 it will execute block of statement
-                            empHrs = 4;
-                            // Console.WriteLine("{0} day Employee is working part time", day);
-                            break;
-                        default:
-                            empHrs = 0; // If random value is 0 then it will execotr
-                                        // Console.WriteLine("{0} day Employee is absent", day);
-                            break;
-                    }
-                    // Employee wage calculation part
-                    totalEmpHrs += empHrs;
-                    Console.WriteLine("Day {0} is {1} hrs", totalWorkingDays, empHrs);
-                    totalWorkingDays++;
+                    case Is_Full_Time: // If random value is 1 it will execute block of statement
+                        empHrs = 8;
+                        break;
+                    case Is_Part_Time: // If random value is 2 it will execute block of statement
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0; // If random value is 0 then it will execotr
+                        break;
                 }
-                return totalEmpHrs * companyEmpWage.empRatePerHr;
+                // Employee wage calculation part
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day {0} is {1} hrs", totalWorkingDays, empHrs);
+                totalWorkingDays++;
             }
+            return totalEmpHrs * companyEmpWage.empRatePerHr;
         }
     }   
     
