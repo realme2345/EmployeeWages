99be789ea8802f5983b8713c5e4087cd4e14d9ba
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-23 20:05:26 +0530

    [Raja] Added UC13EmployeeWage implimented in EmployeeWages

--- EmployeeWages/EmployeeWage.cs
+++ EmployeeWages/EmployeeWage.cs
@@ -34,38 +34,33 @@ namespace EmployeeWages
         }
         private int computeEmpWage( CompanyEmpWage companyEmpWage)
         {
-            {
-                int empHrs = 0,totalEmpHrs= 0,totalWorkingDays = 1,DailyWage=0;
-                while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
-                {
-                    Random random = new Random(); //Creating a object for random value
-                    int empCheck = random.Next(0, 3);// To get the values from 0 to 2
-                    switch (empCheck)
-                    {
-                        case Is_Full_Time: // If random value is 1 it will execute block of statement
-                            empHrs = 8;
-                            // Console.WriteLine("{0} day Employee is working full time", day);
-                            break;
-                        case Is_Part_Time: // If random value is 2 it will execute block of statement
-                            empHrs = 4;
-                            // Console.WriteLine("{0} day Employee is working part time", day);
-
-                            break;
-                        default:
-                            empHrs = 0; // If random value is 0 then it will execotr
-                                        // Console.WriteLine("{0} day Employee is absent", day);
-                            break;
-                    }
+            
+             int empHrs = 0,totalEmpHrs= 0,totalWorkingDays = 1,DailyWage=0;
+             while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+             {
+                  Random random = new Random(); //Creating a object for random value
+                  int empCheck = random.Next(0, 3);// To get the values from 0 to 2
+                  switch (empCheck)
+                  {
+                      case Is_Full_Time: // If random value is 1 it will execute block of statement
+                          empHrs = 8;
+                          break;
+                      case Is_Part_Time: // If random value is 2 it will execute block of statement
+                          empHrs = 4;
+                          break;
+                      default:
+                          empHrs = 0; // If random value is 0 then it will execotr
+                          break;
+                  }
                     // Employee wage calculation part
-                    totalEmpHrs += empHrs;
-                    Console.WriteLine("Day {0} is {1} hrs", totalWorkingDays, empHrs);
-                    totalWorkingDays++;
-                    DailyWage = empHrs *companyEmpWage.empRatePerHr;
-                    int[] total = {DailyWage};          // Creting an arry for to store the Daily employee wage
-                    Console.WriteLine("Daily Wage of Employee: {0}",total[0]);
-                }
-                return totalEmpHrs * companyEmpWage.empRatePerHr;
-            }
+                  totalEmpHrs += empHrs;
+                  Console.WriteLine("Day {0} is {1} hrs", totalWorkingDays, empHrs);
+                  totalWorkingDays++;
+                  DailyWage = empHrs *companyEmpWage.empRatePerHr;
+                  int[] total = {DailyWage};          // Creting an arry for to store the Daily employee wage
+                  Console.WriteLine("Daily Wage of Employee: {0}",total[0]);
+             }
+            return totalEmpHrs * companyEmpWage.empRatePerHr;
         }
     }   
     
